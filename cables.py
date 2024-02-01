c55d50eaac6f751e77be7f613a8b042f02be17c1
Anatoliy-Bezkrovnyi <bezkrovnyi1@gmail.com>
2024-02-01 22:18:24 +0200

    hw_08_adjustments

--- cables.py
+++ cables.py
@@ -1,6 +1,8 @@
 import heapq
 
-cables_length = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
+#cables_length = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
+cables_length = [1,2,4,10]
+cost = []
 
 heapq.heapify(cables_length)
 
@@ -8,5 +10,8 @@ while len(cables_length) != 1:
     min_1 = heapq.heappop(cables_length) 
     min_2 = heapq.heappop(cables_length)
     sum_min = min_1 + min_2
+    cost.append(sum_min)
     heapq.heappush(cables_length, sum_min)
     print(cables_length)
+print(cost)
+print(sum(cost))
